402e6b15ec591f74af94baa8043a5a9dc4be0b9c
a2k-Oleksii <a2k-oleksii@ukr.net>
2022-10-10 14:44:37 +0300

    Delete parsing and add ping in file main.py. Add instalation ping util in Dockerfile

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import json
-import urllib.request
+import os
 
-api_git_request = urllib.request.urlopen("http://api.github.com")
-dict_api = json.loads(api_git_request.read())
-
-for key, el in dict_api.items():
-    print(key, "=====>", el)
+print(os.system("ping -c 4 api.github.com"))
